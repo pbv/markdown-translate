5ecfae03c88d6466f8de36ff88e7b4c22be5eb0e
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2026-05-15 13:36:20 +0100

    cleaup main

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Translate
 import FileIO
 import Options.Applicative
 import Control.Monad (when)
+import System.Directory (doesFileExist)
 
 main :: IO ()
 main = execParser opts >>= runner
@@ -16,15 +17,22 @@ main = execParser opts >>= runner
                      header "Automatically translate Pandoc Markdown files using DeepL.")
 
 runner :: Config -> IO ()
-runner config = mapM_ (translateFile config) (files config)
+runner config = mapM_ (translateFile config) config.files
 
 translateFile :: Config -> FilePath -> IO ()
 translateFile config path = do
   let path' = addLanguage (targetLang config) path
-  when (config.verbosity >= 1) $
-    putStrLn ("*** Translating " ++ path ++ " to " ++ path')
-  doc <- readMarkdownFile path
-  translated <- translatePandoc config doc
-  writeMarkdownFile path' translated
-  when (config.verbosity >= 1) $
-    putStrLn ("*** Wrote " ++ path')
+  check <- doesFileExist path'
+  if check then 
+    when (config.verbosity >= 1) $
+      putStrLn ("*** Destination file "
+                 <> path' <>
+                 " already exists; skipping.")
+    else do
+    when (config.verbosity >= 1) $
+      putStrLn ("*** Translating file " <> path <> " to " <> path')
+    doc <- readMarkdownFile path
+    translated <- translatePandoc config doc
+    writeMarkdownFile path' translated
+    when (config.verbosity >= 1) $
+      putStrLn ("*** Wrote file " <> path')
